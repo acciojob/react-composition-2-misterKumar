a2e80ba410387d303a53e66a4b2ef9a68f029af0
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-14 14:28:03 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,33 @@
-import React, { useState, useRef, useEffect } from "react";
-import "./../styles/App.css";
- 
-const Modal = ({ show, onClose }) => {
-  const modalRef = useRef();
- 
-  const handleOutsideClick = (e) => {
-    if (modalRef.current && !modalRef.current.contains(e.target)) {
-      onClose();
-    }
-  };
- 
-  useEffect(() => {
-    document.addEventListener("click", handleOutsideClick);
- 
-    return () => {
-      document.removeEventListener("click", handleOutsideClick);
-    };
-  }, [onClose]);
- 
-  if (!show) {
-    return null;
-  }
- 
-  return (
-    <div className="model-overlay model" ref={modalRef} style={{ border: "2px solid black" }}>
-      <button className="model-close" onClick={onClose}>
-        Close
-      </button>
-      <p className="modal-p">This is the content of the modal.</p>
-    </div>
-  );
-};
- 
+
+import React,{useState} from "react";
+import './../styles/App.css';
+import Modal from "./Modal";
+
+
 const App = () => {
-  const [show, setShow] = useState(false);
- 
-  const handleClose = () => {
-    setShow(false);
-  };
- 
+
+  let[show,setShow]=useState(false)
+
+  const displayModal=()=>{
+
+    setShow(true)
+  }
+
+
+  const onClose=()=>{
+
+    setShow(false)
+  }
   return (
     <div>
-      <button className="show" onClick={() => setShow(true)}>
-        Show Modal
-      </button>
-      {show && <Modal show={show} onClose={handleClose} />}
-      {/* Do not remove the main div */}
+        {/* Do not remove the main div */}
+        <button onClick={displayModal}>Show Modal</button>
+        { show ?
+        <Modal show={show} onClose={onClose}/>
+        : ""
+        }
     </div>
-  );
-};
- 
-export default App;
+  )
+}
+
+export default App
